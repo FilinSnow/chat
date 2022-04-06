d417aa316994a38ae12d95e77e71f3731af7952c
Denis Kikot <dkikot.exceedteam@gmail.com>
2022-04-06 10:26:46 +0300

    container styles + voice command

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,4 +1,13 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react'
+import 
+  React, 
+  { 
+    useCallback, 
+    useContext, 
+    useEffect, 
+    useState,
+    useRef,
+    useLayoutEffect
+  } from 'react';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { collection, doc, setDoc } from "firebase/firestore";
 import { Context } from '../..';
@@ -7,20 +16,23 @@ import './Chat.scss'
 
 
 const Chat = () => {
-  const { db }: any = useContext(Context)
+  const { db }: any = useContext(Context);
   const [value, setValue] = useState('');
   const [flag, setFlag] = useState(false);
-  const tmpUser: any = localStorage.getItem('user')
-  const user = JSON.parse(tmpUser)
-  const [messages] = useCollectionData(
+  const tmpUser: any = localStorage.getItem('user');
+  const user = JSON.parse(tmpUser);
+  const [messages = []] = useCollectionData(
     collection(db, 'messages')
-  )
-  const messagesRef = collection(db, 'messages')
+  );
+  const messagesRef = collection(db, 'messages');
+  const messageRef = useRef<null | HTMLDivElement>(null);
 
   const sendMessage = useCallback(async () => {
-    const index = `${Date.now()}`
-    const regular = /^[а-яА-Яa-zA-Z0-9()*_\-!#$%^&*,."'\][]*$/
-    setValue('')
+    const index = `${Date.now()}`;
+    const regular = /^[а-яА-Яa-zA-Z0-9()*_\-!#$%^&*,."'\][]*$/;
+
+    setValue('');
+    
     if(!regular.test(value)) {
       return 
     }
@@ -50,25 +62,71 @@ const Chat = () => {
     };
   }, [value, sendMessage]);
 
+  useEffect(() => {
+    const len = messages.length;
+
+    if (len > 0) {
+      if (messages[len - 1].text === '!sound') play();
+    }
+
+    // setTimeout(() => {
+    //   if (messageRef.current) {
+    //     messageRef.current.scrollIntoView(
+    //       {
+    //         behavior: 'smooth',
+    //         block: 'end',
+    //         inline: 'nearest'
+    //       })
+    //   }
+    // }, 1500);
+
+  }, [messages])
+  console.log(messages)
+
+  const play = () => {
+    const audio = new Audio("https://notificationsounds.com/storage/sounds/file-sounds-1303-man-its-for-you.ogg");
+    audio.play();
+  }
+
+  useLayoutEffect(() => {
+    if (messageRef.current) {
+      console.log(1111)
+      messageRef.current.scrollIntoView(
+        {
+          behavior: 'smooth',
+          // block: 'end',
+          // inline: 'nearest'
+        })
+    }
+  }, [messages])
+
+
   return (
     <div>
       <h3>Chat</h3>
-      <div className='wrapper__chat' style={{background: sky}}>
+      <div ref={messageRef} className='wrapper__chat' style={{background: sky}}>
         <div style={{ margin: '0 auto', display: 'flex', flexDirection: 'column'}}>
           {messages && messages.map(message => {
+            const {createdAt, uid, displayName, text, photoURL} = message;
+
             return (
-              <div key={message?.createdAt} className='message' style={{display: 'flex', justifyContent: message?.uid === user?.uid ? 'end' : 'start' }}>
-                <div style={{ background: 'lightGray', margin: 5, maxWidth: 250, padding: 5}}>
-                  <div style={{display: 'flex', justifyContent: 'flex-start'}}>name: {message?.displayName}</div>
-                  <div style={{display: 'flex', justifyContent: 'flex-start'}}>text: {message?.text}</div>
+              <div 
+                key={createdAt} 
+                className={uid === user?.uid ? 'message' : 'message-owner'}
+              >
+                <div className='message-content'>
+                  <div>name: {displayName}</div>
+                  <div>text: {text}</div>
                 </div>
               </div>
             )
           })}
         </div>
       </div>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick={() => sendMessage()}>Sent</button>
+      <div className='send-message'>
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+        <button onClick={() => sendMessage()}>Send</button>
+      </div>
     </div>
   )
 }
